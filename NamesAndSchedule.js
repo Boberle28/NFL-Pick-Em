fcc4a162acb18b3f6403b19caef4b1ae305e3cd5
Brandon <brando2885@hotmail.com>
2025-07-30 20:35:56 -0500

    ok

--- NamesAndSchedule.js
+++ NamesAndSchedule.js
@@ -999,6 +999,12 @@ week18.AddGame(HOU, IND);
       return players;
   }
 
+  function FindUserAllLowercase(name){
+    return Object.values(players).find(player =>
+      player.username.toLowerCase() === name.toLowerCase()
+    );
+  }
+
   function getCurrentWeek(currentDate, seasonjson) {
     const now = new Date(currentDate);
 
