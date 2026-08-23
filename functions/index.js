4ec3eb60f4408f8e85d0035680bbc739b909df45
Brandon Oberle <brando2885@hotmail.com>
2026-08-23 10:46:25 -0500

    enabling could functions with firebase

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+
+// For cost control, you can set the maximum number of containers that can be
+// running at the same time. This helps mitigate the impact of unexpected
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
+setGlobalOptions({ maxInstances: 10 });
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+
+const lockTimes = {
+    week1: "2026-09-10T00:20:00Z",
+    week2: "2026-09-18T00:15:00Z",
+    week3: "2026-09-25T00:15:00Z",
+    week4: "2026-10-02T00:15:00Z",
+    week5: "2026-10-09T00:15:00Z",
+    week6: "2026-10-16T00:15:00Z",
+    week7: "2026-10-23T00:15:00Z",
+    week8: "2026-10-30T00:15:00Z",
+    week9: "2026-11-06T01:15:00Z",
+    week10: "2026-11-13T01:15:00Z",
+    week11: "2026-11-20T01:15:00Z",
+    week12: "2026-11-26T01:00:00Z",
+    week13: "2026-12-04T01:15:00Z",
+    week14: "2026-12-11T01:15:00Z",
+    week15: "2026-12-18T01:15:00Z",
+    week16: "2026-12-25T01:15:00Z",
+    week17: "2027-01-01T01:15:00Z",
+    week18: "2027-01-10T18:00:00Z"
+};
