2ae70e87f24d321a85e0a6bbd275fdb9f815011c
Brandon Oberle <brando2885@hotmail.com>
2026-09-06 12:17:09 -0500

    trouble shooting

--- NamesAndSchedule.js
+++ NamesAndSchedule.js
@@ -1500,7 +1500,7 @@ week18.AddGame(HOU, TEN);
 
     //const allWeeks = [...season.preseasonWeeks, ...season.weeks];
 
-    if(season.weeks[0].startDate > now)
+    if(season.weeks[0].startDate <= now)
     {
       console.log("Returning season.weeks[0]!");
       console.log("season.weeks[0].startDate", season.weeks[0].startDate);
