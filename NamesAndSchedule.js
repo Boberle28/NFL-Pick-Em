770345dd964d85cc53880d9246ce7ee579d83e43
Boberle28 <80209849+Boberle28@users.noreply.github.com>
2026-09-15 08:17:01 -0500

    Update NamesAndSchedule.js
    Fixed first week keeps showing up bug

--- NamesAndSchedule.js
+++ NamesAndSchedule.js
@@ -1500,14 +1500,6 @@ week18.AddGame(HOU, TEN);
 
     const allWeeks = [...season.preseasonWeeks, ...season.weeks];
 
-    if(season.weeks[0].startDate <= now)
-    {
-      console.log("Returning season.weeks[0]!");
-      console.log("season.weeks[0].startDate", season.weeks[0].startDate);
-      console.log("now", now);
-      return season.weeks[0];
-    }
-
     for (const week of season.weeks) {
       if (now >= week.startDate && now <= week.endDate) {
         console.log("Returning week!", week);
